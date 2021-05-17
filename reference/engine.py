726bc0721451d70a0645a6e7eb2ae47ac53fd7d3
seungwoos <tmddngood@gmail.com>
2021-05-17 10:47:40 +0900

    fixed to adapt our project

--- reference/engine.py
+++ reference/engine.py
@@ -5,10 +5,9 @@ import torch
 
 import torchvision.models.detection.mask_rcnn
 
-from coco_utils import get_coco_api_from_dataset
-from coco_eval import CocoEvaluator
-import utils
-
+from .coco_utils import get_coco_api_from_dataset
+from .coco_eval import CocoEvaluator
+from . import utils
 
 def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
     model.train()
@@ -28,13 +27,11 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
         loss_dict = model(images, targets)
-
-        losses = sum(loss for loss in loss_dict.values())
-
+        losses = sum(loss.mean() for loss in loss_dict.values())
+        
         # reduce losses over all GPUs for logging purposes
         loss_dict_reduced = utils.reduce_dict(loss_dict)
-        losses_reduced = sum(loss for loss in loss_dict_reduced.values())
-
+        losses_reduced = sum(loss.mean() for loss in loss_dict_reduced.values())
         loss_value = losses_reduced.item()
 
         if not math.isfinite(loss_value):
