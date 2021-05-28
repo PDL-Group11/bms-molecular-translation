14f120f9169e698d1a1dbe92d589280d714f37ba
seungwoos <tmddngood@gmail.com>
2021-05-28 22:15:40 +0900

    fixed minor errata

--- swin_transformer.py
+++ swin_transformer.py
@@ -625,7 +625,7 @@ class SwinTransformer(nn.Module):
     def train(self, mode=True):
         """Convert the model into training mode while keep layers freezed."""
         super(SwinTransformer, self).train(mode)
-        self._freeze_stages()\
+        self._freeze_stages()
 
 
 
